3d28a2d90990719b9fef2a785b830f082b358109
VENUGOPALBOBBADI <21eg106b07@anurag.edu.in>
2023-09-26 07:58:02 +0530

    new

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose'); 
-require('./locations');
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+var gracefulShutdown;
 var dbURI = "mongodb://0.0.0.0:27017/"; 
+if(process.env.NODE_ENV==='production'){
+    dbURI = process.env.MONGODB_URI;
+}
 mongoose.connect(dbURI); 
 mongoose.connection.on('connected', function () { 
     console.log('Mongoose connected to ' + dbURI); 
@@ -11,3 +16,5 @@ mongoose.connection.on('error', function (err) {
 mongoose.connection.on('disconnected', function () { 
     console.log('Mongoose disconnected'); 
 }); 
+
+require('./locations');
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,7 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const router = express.Router();
-const ctrlLocations =
-require('../controllers/locations');
+const ctrlLocations = require('../controllers/locations');
 const ctrlOthers = require('../controllers/others');
 /* Locations pages */
 router.get('/', ctrlLocations.homelist);
